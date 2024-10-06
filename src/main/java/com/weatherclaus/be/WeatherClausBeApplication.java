158eea594b81a0fac2fc866c481901d21f322a29
cloud <goorm94@naver.com>
2024-10-06 21:40:33 +0900

    chore: Initialize Spring Boot project
    Added initial Spring Boot project structure with basic dependencies and configurations.

---
+++ src/main/java/com/weatherclaus/be/WeatherClausBeApplication.java
@@ -0,0 +1,13 @@
+package com.weatherclaus.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherClausBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherClausBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weatherclaus/be/WeatherClausBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherclaus.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherClausBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
