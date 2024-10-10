1ff4d75ef724adb4db8bff9ec2d201ce2f858c71
cloud <goorm94@naver.com>
2024-10-10 09:43:24 +0900

    Refactor: WeatherController WeatherService 리팩토링
    - 가독성 향상 및 로직 정리

--- src/main/java/com/weatherclaus/be/weather/controller/WeatherController.java
+++ src/main/java/com/weatherclaus/be/weather/controller/WeatherController.java
@@ -1,56 +1,62 @@
 package com.weatherclaus.be.weather.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.weatherclaus.be.weather.dto.ResponseDto;
+import com.weatherclaus.be.weather.dto.LatLonDTO;
+import com.weatherclaus.be.weather.dto.WeatherResponse;
 import com.weatherclaus.be.weather.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(("/api/weather"))
 public class WeatherController {
 
     private final WeatherService weatherService;
 
-    @GetMapping("/weather")
-    public String getWeather(
+    private static final double DEFAULT_LAT = 37.5806949;
+    private static final double DEFAULT_LON = 126.9827989;
+
+
+    @GetMapping("/forecast")
+    public ResponseEntity<ResponseDto<?>> getWeather(
             @RequestParam(required = false, defaultValue = "0") Double lat,
             @RequestParam(required = false, defaultValue = "0") Double lon,
             @RequestParam(required = false) String city) throws JsonProcessingException {
 
-//        위 경도로 넘어왔을 때
-        if(lat != 0 && lon != 0) {
+        //도시이름 기반
+        if(city != null) {
 
-            // 캐시 키 생성
-            String cacheKey = weatherService.createCacheKey(lat, lon);
+            LatLonDTO latLon = weatherService.getLatLon(city);
 
-            // WeatherService를 통해 캐시 처리
-            return weatherService.getWeather(cacheKey, lat, lon);
+            lat = latLon.getLat();
 
-//         도시 이름이 넘어왔을 때
-        }else if(city != null) {
+            lon = latLon.getLon();
 
+        //디폴트 값
+        }else if(lat == 0 && lon == 0){
 
-            String[] split = weatherService.getLatLon(city).split(",");
+            lat = DEFAULT_LAT;
 
-            lat = Double.parseDouble(split[0]);
-            lon = Double.parseDouble(split[1]);
+            lon = DEFAULT_LON;
 
-
-
-            String cacheKey = weatherService.createCacheKey(lat, lon);
-
-            return weatherService.getWeather(cacheKey, lat, lon);
-
-//            아무것도 입력받지 않은 기본값.
-        }else{
-
-            return weatherService.getWeather("hihi", 37.2633325, 127.0287472);
         }
+
+
+        String cacheKey = weatherService.createCacheKey(lat, lon);
+
+
+        WeatherResponse weatherResponse = weatherService.getWeather(cacheKey, lat, lon);
+
+        return new ResponseEntity<>(
+                new ResponseDto<>("success", "Data retrieved successfully", weatherResponse, null, 200),
+                HttpStatus.OK
+        );
     }
 }
--- src/main/java/com/weatherclaus/be/weather/service/WeatherService.java
+++ src/main/java/com/weatherclaus/be/weather/service/WeatherService.java
@@ -3,24 +3,32 @@ package com.weatherclaus.be.weather.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.weatherclaus.be.weather.dto.WeatherDTO;
+import com.weatherclaus.be.weather.dto.LatLonDTO;
+import com.weatherclaus.be.weather.dto.WeatherResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Map;
+
 
 @Service
+@RequiredArgsConstructor
 public class WeatherService {
 
-    private static final String WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/forecast?lang=kr&units=metric&lat=%s&lon=%s&appid=%s";
+    private final ObjectMapper objectMapper;
 
-    private static final String GEOCODING_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s";
+    @Value("${WEATHER_API_URL}")
+    private String WEATHER_API_URL;
+
+    @Value("${GEOCODING_API_URL}")
+    private String GEOCODING_API_URL;
 
     @Value("${OPENWEATHERMAP_API_KEY}")
-    private String apiKey = "your-api-key";
+    private String apiKey;
+
 
     // 3km 범위로 캐시 키 생성 메서드
     public String createCacheKey(double lat, double lon) {
@@ -29,41 +37,34 @@ public class WeatherService {
         return gridLat + "," + gridLon;
     }
 
-//    @Cacheable(value = "weatherCache", key = "#cacheKey")
-    public String getWeather(String cacheKey, double lat, double lon) {
+    @Cacheable(value = "weatherCache", key = "#cacheKey")
+    public WeatherResponse getWeather(String cacheKey, double lat, double lon) throws JsonProcessingException {
         // Weather API 호출
         String url = String.format(WEATHER_API_URL, lat, lon, apiKey);
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(url, String.class);
-    }
-
-
-    @Cacheable(value = "cityCache", key = "#city")
-    public String getLatLon(String city) throws JsonProcessingException {
-
-        String url = String.format(GEOCODING_API_URL, city, apiKey);
         RestTemplate restTemplate = new RestTemplate();
         String jsonData = restTemplate.getForObject(url, String.class);
 
 
 
 
+        return objectMapper.readValue(jsonData, WeatherResponse.class);
+    }
 
-        // ObjectMapper 사용하여 JSON을 List<LocationDto>로 변환
-        ObjectMapper mapper = new ObjectMapper();
-        List<WeatherDTO> locations = mapper.readValue(jsonData, new TypeReference<List<WeatherDTO>>() {
-        });
 
-        if(locations == null || locations.isEmpty()) {
-            throw new IllegalArgumentException("잘못된 인자 ");
+    @Cacheable(value = "cityCache", key = "#city")
+    public LatLonDTO getLatLon(String city) throws JsonProcessingException {
+
+        String url = String.format(GEOCODING_API_URL, city, apiKey);
+        RestTemplate restTemplate = new RestTemplate();
+        String jsonData = restTemplate.getForObject(url, String.class);
+
+        List<LatLonDTO> weatherDTOList = objectMapper.readValue(jsonData, new TypeReference<>() {});
+
+        if (weatherDTOList == null || weatherDTOList.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 도시이름");
         }
 
-        WeatherDTO location = locations.get(0);  // 첫 번째 결과 사용
-//        Double[] arr = new Double[2];
-//        arr[0] = location.getLat();
-//        arr[1] = location.getLon();
-
-        return location.getLat() + "," + location.getLon();
+        return weatherDTOList.get(0);
     }
 
 
