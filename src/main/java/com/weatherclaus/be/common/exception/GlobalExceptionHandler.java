bd863e994c183fe89019fe8f7a8ebb41edfe8fb3
cloud <goorm94@naver.com>
2024-11-01 21:12:40 +0900

    Refactor: Integration ExceptionHandler
    - Integrate Some ExceptionHandler to One
    - Add log to errors

--- src/main/java/com/weatherclaus/be/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/weatherclaus/be/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.weatherclaus.be.common.exception;
 
 import com.weatherclaus.be.common.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Order(2)  // 높은 우선순위
+@Slf4j
 public class GlobalExceptionHandler {
 
 
@@ -17,7 +19,9 @@ public class GlobalExceptionHandler {
     // IllegalArgumentException 처리
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ResponseDto<?>> handleIllegalArgumentException(IllegalArgumentException e) {
-        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Invalid Argument", e.getMessage());
+        log.error("IllegalArgumentException: {}", e.getMessage(), e);
+
+        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Invalid Argument", "유효하지 않은 요청입니다.");
 
         return new ResponseEntity<>(
                 new ResponseDto<>("fail", "Invalid argument", null, errorDetails, 400),
@@ -27,7 +31,9 @@ public class GlobalExceptionHandler {
     // NullPointerException 처리
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<ResponseDto<?>> handleNullPointerException(NullPointerException e) {
-        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Null Value", "A required value was null");
+        log.error("NullPointerException: {}", e.getMessage(), e);
+
+        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Null Value", "유효하지 않은 값입니다.");
 
         return new ResponseEntity<>(
                 new ResponseDto<>("fail", "Null pointer exception", null, errorDetails, 500),
@@ -36,7 +42,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseDto<?>> handleException(Exception e) {
-        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", e.getMessage());
+        log.error("Exception: {}", e.getMessage(), e);
+        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", "잘못된 요청입니다.");
 
         // 실패 시 400 상태 코드로 에러 응답
         return new ResponseEntity<>(
--- src/main/java/com/weatherclaus/be/user/exception/UserExceptionHandler.java
+++ src/main/java/com/weatherclaus/be/user/exception/UserExceptionHandler.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Order(1)  // 높은 우선순위
+@Slf4j
 public class UserExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDto<?>> handleValidationExceptions(MethodArgumentNotValidException e) {
+
         BindingResult bindingResult = e.getBindingResult();
 
 
@@ -29,54 +31,22 @@ public class UserExceptionHandler {
 
         ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", errorMessage);
 
+        log.error("MethodArgumentNotValidException: {}", errorMessage, e);
 
 
-        return new ResponseEntity<>(
-                new ResponseDto<>("fail", "Invalid request", null, errorDetails, 410),
-                HttpStatus.BAD_REQUEST);
-    }
 
-    @ExceptionHandler(PasswordMismatchException.class)
-    public ResponseEntity<ResponseDto<?>> handlePasswordMismatchException(PasswordMismatchException e) {
-        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", e.getMessage());
 
-        // 실패 시 400 상태 코드로 에러 응답
-        return new ResponseEntity<>(
-                new ResponseDto<>("fail", "Invalid request", null, errorDetails, 400),
-                HttpStatus.BAD_REQUEST);        }
-
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<ResponseDto<?>> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
-        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", e.getMessage());
-
-        // 실패 시 400 상태 코드로 에러 응답
         return new ResponseEntity<>(
                 new ResponseDto<>("fail", "Invalid request", null, errorDetails, 400),
                 HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<ResponseDto<?>> EmailAlreadyExistsException(EmailAlreadyExistsException e) {
-        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", e.getMessage());
 
-        // 실패 시 400 상태 코드로 에러 응답
-        return new ResponseEntity<>(
-                new ResponseDto<>("fail", "Invalid request", null, errorDetails, 400),
-                HttpStatus.BAD_REQUEST);
-    }
 
-    @ExceptionHandler(CodeMismatchException.class)
-    public ResponseEntity<ResponseDto<?>> CodeMismatchException(CodeMismatchException e) {
-        ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", e.getMessage());
-
-        // 실패 시 400 상태 코드로 에러 응답
-        return new ResponseEntity<>(
-                new ResponseDto<>("fail", "Invalid request", null, errorDetails, 400),
-                HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler({RecaptchaTokenInvalidException.class, UsernameNotFoundException.class, AuthenticationNotValid.class,EmailNotFoundException.class})
+    @ExceptionHandler({RecaptchaTokenInvalidException.class, UsernameNotFoundException.class, AuthenticationNotValid.class,EmailNotFoundException.class, CodeMismatchException.class,EmailAlreadyExistsException.class,UserAlreadyExistsException.class,PasswordMismatchException.class})
     public ResponseEntity<ResponseDto<?>> RecaptchaTokenInvalidCheck(Exception e) {
+        log.error("Exception: {}", e.getMessage(), e);
+
         ResponseDto.ErrorDetails errorDetails = new ResponseDto.ErrorDetails("Bad Request", e.getMessage());
 
         // 실패 시 400 상태 코드로 에러 응답
