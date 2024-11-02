a103e1d50c26b704ddbac75e62ed9cdf7dfe1cca
cloud <goorm94@naver.com>
2024-11-02 11:07:54 +0900

    Refactor: PageableDefault,JsonFormat
    - Fix PageSize 10, Validation PageNumber
    - Add Date Json Format

--- src/main/java/com/weatherclaus/be/websocket/dto/ChatListResponse.java
+++ src/main/java/com/weatherclaus/be/websocket/dto/ChatListResponse.java
@@ -16,7 +16,7 @@ public class ChatListResponse {
 
     private String message;
 
-    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime sentDate;
     private String nickname;
     private String imageUrl;
